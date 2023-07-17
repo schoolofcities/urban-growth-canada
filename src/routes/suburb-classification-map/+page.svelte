b06a97582820f1a3b8655dc18dc50208d41a75d1
Remus-H <remus.herteg@mail.utoronto.ca>
2023-07-17 18:39:07 -0400

    Early functionality
    Parsed the cma name and cma coordinates into separate arrays for the search bar
    
    Removed some other functionalities like the legend and changing colours on-click in order to read code better but that can be added later

--- src/routes/suburb-classification-map/+page.svelte
+++ src/routes/suburb-classification-map/+page.svelte
@@ -1,35 +1,94 @@
 <script>
 	import { onMount } from 'svelte'
 	import mapboxgl from "mapbox-gl";
-	import '../../assets/global-styles.css';
+    import raw from 'C:/Users/Remus/Desktop/School/Work Study/3-Website/urban-growth-canada/src/routes/suburb-classification-map/data/cma-summary.json'
+    //import Typeahead from 'svelte-typeahead/types/Typeahead.svelte';
+	//import '../../assets/global-styles.css';
 
 	mapboxgl.accessToken = 'pk.eyJ1Ijoic2Nob29sb2ZjaXRpZXMiLCJhIjoiY2w2Z2xhOXprMTYzczNlcHNjMnNvdGlmNCJ9.lOgVHrajc1L-LlU0as2i2A';
 
 	// global cma variable
 	let cma = 'Toronto';
+    let map;
 
+    const name = raw.map(cityname => cityname.cmaname);
+    const cords = raw.map(citycord => ({ x: citycord.x , y: citycord.y}));
 
+console.log(cords)
+console.log(name)
+
+//Map and its elements
 	onMount(() => {
 		map = new mapboxgl.Map({
 			container: 'map', 
 			style: 'mapbox://styles/schoolofcities/cli0otj3n04m601pa9s0s0mc4',
-			center: [-79.45, 43.65], 
+			center: [-73.628, 45.575], 
 			zoom: 10,
-			maxZoom: 12,
+			maxZoom: 11,
 			minZoom: 1,
-			projection: 'globe',
 			scrollZoom: true,
 			attributionControl: false
 		});
-		
-		map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+	// Knick knacks	
+	map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
 		const scale = new mapboxgl.ScaleControl({
 			maxWidth: 100,
 			unit: 'metric'
 			});
-		map.addControl(scale, 'bottom-right');
-	});
+
+	map.addControl(scale, 'bottom-right');
+
+
+    // Mouse functions
+    map.on('mouseenter', 'suburbs-project-ct', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    map.on('mouseleave', 'suburbs-project-ct', () => {
+        map.getCanvas().style.cursor = '';
+    });
+
+    map.on('click', 'suburbs-project-ct', (e) => {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML("CMA: " + e.features[0].properties.cmaname) //indexes the GeoJSON code for the CTUID from properties
+            .addTo(map);
+    });
+
+
+/*  
+map.on('click', 'suburbs-project-ct', (e) => {		
+
+    var features = map.queryRenderedFeatures(e.point, { layers: ['suburbs-project-ct'] });
+
+    if (ctuid != features[0].properties.ctuid) {
+        var style = [
+            "match",
+            ["get", "CTUID"],
+            [features[0].properties.ctuid],
+            "#f1c500",
+            "#fff"
+        ]
+        map.setPaintProperty('suburbs-project-ct', 'fill-color', style)
+        ctuid = features[0].properties.ctuid
+        } 
+    else {
+    map.setPaintProperty('suburbs-project-ct', 'fill-color', '#fff')
+    ctuid = '0'
+    }
+
+});
+
+function placeClick(city) {
+		cmaname = "0";
+		map.setPaintProperty('ct_fill', 'fill-color', '#fff');
+		map.panTo(city.original.geometry.coordinates);
+		map.zoonTo(10);
+	}
+*/
+
+});
 
 </script>
 
@@ -39,13 +98,28 @@
 </svelte:head>
 
 
-
 <main>
 
 	<div id="map"></div>
 
+<!--
+    <div id="search">
+        <Typeahead
+                {data}
+                label=""
+                inputAfterSelect="clear"
+                placeholder={`search and zoom to a cma`}
+                limit={5}
+                extract={(item) => item.properties.cmaname}
+                on:select={({ detail }) => placeClick(detail)}
+                let:result
+            >
+            <span id="search-results">{@html result.string}</span>
+        </Typeahead>
+    
+    </div>
+-->
 </main>
-
 
 
 <style>
