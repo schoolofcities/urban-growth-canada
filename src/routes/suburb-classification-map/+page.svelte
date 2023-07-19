79e69714e53ef72cd0c01bb283baf301c360480c
Jeff Allen <jeffallen89@gmail.com>
2023-07-19 13:46:41 -0400

    add in drop down list and select

--- src/routes/suburb-classification-map/+page.svelte
+++ src/routes/suburb-classification-map/+page.svelte
@@ -1,60 +1,74 @@
 <script>
 	import { onMount } from 'svelte'
 	import mapboxgl from "mapbox-gl";
-    import raw from 'C:/Users/Remus/Desktop/School/Work Study/3-Website/urban-growth-canada/src/routes/suburb-classification-map/data/cma-summary.json'
-    //import Typeahead from 'svelte-typeahead/types/Typeahead.svelte';
-	//import '../../assets/global-styles.css';
+    import cmaSummary from './data/cma-summary.json';
+    import Typeahead from "svelte-typeahead";
+    import Select from 'svelte-select';
+	import '../../assets/global-styles.css';
 
 	mapboxgl.accessToken = 'pk.eyJ1Ijoic2Nob29sb2ZjaXRpZXMiLCJhIjoiY2w2Z2xhOXprMTYzczNlcHNjMnNvdGlmNCJ9.lOgVHrajc1L-LlU0as2i2A';
 
 	// global cma variable
-	let cma = 'Toronto';
+    let cmaAll = cmaSummary
+        .sort((a, b) => b.pop2021 - a.pop2021)
+        .map(item => item.cmaname);
+        
+	let cmaSelected = 'Montreal';
+    $: console.log(cmaSelected);
+
+    function handleSelect(e) {
+        cmaSelected = e.detail.value;
+    }
+
+
+
+
+
+
+
+
     let map;
 
-    const name = raw.map(cityname => cityname.cmaname);
-    const cords = raw.map(citycord => ({ x: citycord.x , y: citycord.y}));
 
-console.log(cords)
-console.log(name)
+    // const name = raw.map(cityname => cityname.cmaname);
+    // const cords = raw.map(citycord => ({ x: citycord.x , y: citycord.y}));
 
-//Map and its elements
-	onMount(() => {
-		map = new mapboxgl.Map({
-			container: 'map', 
-			style: 'mapbox://styles/schoolofcities/cli0otj3n04m601pa9s0s0mc4',
-			center: [-73.628, 45.575], 
-			zoom: 10,
-			maxZoom: 11,
-			minZoom: 1,
-			scrollZoom: true,
-			attributionControl: false
-		});
+    onMount(() => {
+        map = new mapboxgl.Map({
+            container: 'map', 
+            style: 'mapbox://styles/schoolofcities/cli0otj3n04m601pa9s0s0mc4',
+            center: [-73.628, 45.575], 
+            zoom: 10,
+            maxZoom: 11,
+            minZoom: 1,
+            scrollZoom: true,
+            attributionControl: false
+        });
 	// Knick knacks	
-	map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+        map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
-		const scale = new mapboxgl.ScaleControl({
-			maxWidth: 100,
-			unit: 'metric'
-			});
+        const scale = new mapboxgl.ScaleControl({
+            maxWidth: 100,
+            unit: 'metric'
+            });
 
-	map.addControl(scale, 'bottom-right');
+        map.addControl(scale, 'bottom-right');
 
+        // Mouse functions
+        map.on('mouseenter', 'suburbs-project-ct', () => {
+            map.getCanvas().style.cursor = 'pointer';
+        });
 
-    // Mouse functions
-    map.on('mouseenter', 'suburbs-project-ct', () => {
-        map.getCanvas().style.cursor = 'pointer';
-    });
+        map.on('mouseleave', 'suburbs-project-ct', () => {
+            map.getCanvas().style.cursor = '';
+        });
 
-    map.on('mouseleave', 'suburbs-project-ct', () => {
-        map.getCanvas().style.cursor = '';
-    });
-
-    map.on('click', 'suburbs-project-ct', (e) => {
-        new mapboxgl.Popup()
-            .setLngLat(e.lngLat)
-            .setHTML("CMA: " + e.features[0].properties.cmaname) //indexes the GeoJSON code for the CTUID from properties
-            .addTo(map);
-    });
+        map.on('click', 'suburbs-project-ct', (e) => {
+            new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML("CMA: " + e.features[0].properties.cmaname) //indexes the GeoJSON code for the CTUID from properties
+                .addTo(map);
+        });
 
 
 /*  
@@ -100,25 +114,29 @@ function placeClick(city) {
 
 <main>
 
+    <div id="select">
+        <Select 
+            items={cmaAll} 
+            value={cmaSelected} 
+            clearable={false} 
+            showChevron=true 
+            on:input={handleSelect}
+            --background="white"
+            --height="20px"
+            --item-color="black"
+            --border-radius="0"
+            --border="1px"
+            --list-border-radius="0px"
+            --font-size="15px"
+            --max-height="30px"
+            --item-is-active-color="black"
+            --item-is-active-bg="lightgrey"
+
+        />
+    </div>
+      
 	<div id="map"></div>
 
-<!--
-    <div id="search">
-        <Typeahead
-                {data}
-                label=""
-                inputAfterSelect="clear"
-                placeholder={`search and zoom to a cma`}
-                limit={5}
-                extract={(item) => item.properties.cmaname}
-                on:select={({ detail }) => placeClick(detail)}
-                let:result
-            >
-            <span id="search-results">{@html result.string}</span>
-        </Typeahead>
-    
-    </div>
--->
 </main>
 
 
@@ -142,5 +160,23 @@ function placeClick(city) {
 		position: absolute;
 		z-index: -99;
 	}
+
+    #select {
+        width: 250px;
+        height: 30px;
+        position: absolute;
+        top: 5px;
+        left: 5px;
+        background-color: white; 
+        border: solid 1px var(--brandDarkBlue);
+        z-index: 1; 
+    }
+
+    #select p {
+        font-size: 12px;
+        margin: 0px;
+        padding: 0px;
+        padding-left: 20px;
+    }
 	
 </style>
