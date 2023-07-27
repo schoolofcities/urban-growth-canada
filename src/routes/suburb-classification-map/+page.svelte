139cfbc207635c50dd6a3969657c337981cfc81d
Remus-H <remus.herteg@mail.utoronto.ca>
2023-07-26 23:03:53 -0400

    Data functionality and more
    Introduced functionality so that the population and percents show up whenever a specific CMA is chosed.
    Also played around with some CSS and began on a satellite basemap toggle

--- src/routes/suburb-classification-map/+page.svelte
+++ src/routes/suburb-classification-map/+page.svelte
@@ -1,10 +1,10 @@
 <script>
 	import { onMount } from 'svelte'
 	import mapboxgl from "mapbox-gl";
-    import cmaSummary from './data/cma-summary.json';
+    import cmaSummary from './suburb-classification-map/data/cma-summary.json';
     import Typeahead from "svelte-typeahead";
     import Select from 'svelte-select';
-	import '../../assets/global-styles.css';
+	//import '../../assets/global-styles.css';
 
 	mapboxgl.accessToken = 'pk.eyJ1Ijoic2Nob29sb2ZjaXRpZXMiLCJhIjoiY2w2Z2xhOXprMTYzczNlcHNjMnNvdGlmNCJ9.lOgVHrajc1L-LlU0as2i2A';
 
@@ -13,12 +13,26 @@
         .sort((a, b) => b.pop2021 - a.pop2021)
         .map(item => item.cmaname);
         
+    // array of populations
+    let cmaPopulation = cmaSummary
+        .sort((a, b) => b.pop2021 - a.pop2021)
+        .map(item => item.pop2021);
+    
+        
     // initial cma selected
 	let cmaSelected = 'Montreal';
-    $: console.log(cmaSelected);
+    let selectedPop = (4291732).toLocaleString();
+    let selectedActive = (0.172604906*100).toFixed(1);
+    let selectedTransit = (0.131493532*100).toFixed(1);
+    let selectedAuto = (0.663712459*100).toFixed(1);
+    let selectedExurban = (0.032049299*100).toFixed(1);
+
+    $: console.log(cmaPopulation);
 
     // create map variable to fill in with onMount
     let map;
+    let selectedCtuid;
+    let selectedClass;
 
     // function for what to do when new cma is selected
     function handleSelect(e) {
@@ -32,6 +46,13 @@
         let cmaX = filteredData.x;
         let cmaY = filteredData.y;
         let cmauid = filteredData.cmauid.toString();
+        
+        //change data values based on cma selected
+        selectedPop = (filteredData.pop2021).toLocaleString();
+        selectedActive = ((filteredData.active) * 100).toFixed(1);
+        selectedTransit = ((filteredData.transit) * 100).toFixed(1);
+        selectedAuto = ((filteredData.auto) * 100).toFixed(1);
+        selectedExurban = ((filteredData.exurban) * 100).toFixed(1);
 
         // pan and zoom to the new cma - reset pitch and bearing if they changed
         map.setZoom(9);
@@ -60,7 +81,7 @@
                 false
             ]
         );
-    }
+        }
 
     onMount(() => {
         map = new mapboxgl.Map({
@@ -99,7 +120,12 @@
                 .addTo(map);
         });
 
+        map.on('click', 'suburbs-project-ct', (e) => {		
+			map.setPaintProperty('suburbs-project-ct' , 'fill-color' , 'pink')
+        });
+
     });
+
 
 </script>
 
@@ -112,6 +138,7 @@
 <main>
 
     <div id="select">
+        
         <Select 
             items={cmaAll} 
             value={cmaSelected} 
@@ -128,10 +155,34 @@
             --max-height="30px"
             --item-is-active-color="black"
             --item-is-active-bg="lightgrey"
-
         />
+        <div id="data">
+        Population: {selectedPop}
+        <br>Active: {selectedActive}%
+        <br>Transit:{selectedTransit}%
+        <br>Auto: {selectedAuto}%
+        <br>Exurban: {selectedExurban}%
     </div>
-      
+    </div>
+
+    <div id="box">
+         CTUID: {selectedCtuid}
+         <br>Classification: {selectedClass}
+        </div>
+    
+    <div id="slider">
+        
+        Satellite
+        <label class="switch">
+            <input type="checkbox">
+            <span class="slider round"></span>
+          </label>
+
+    </div>
+
+
+
+
 	<div id="map"></div>
 
 </main>
@@ -169,11 +220,105 @@
         z-index: 1; 
     }
 
+    #data {
+        display: flex;
+        font-size: large;
+        width: 250px;
+        background-color: aqua;
+        justify-content: center;
+        align-items: center;
+        border: 2px solid black;
+        
+    }
+    
     #select p {
         font-size: 12px;
         margin: 0px;
         padding: 0px;
         padding-left: 20px;
     }
+
+    #box {
+    position: absolute;
+    font: 14px/20px 'Helvetica Neue', Arial, Helvetica, sans-serif;
+    background: #2b1102;
+    color: rgb(231, 226, 226);
+    width: 260px;
+    top: 650px;
+    left: 1200px;
+    margin-top: 10px;
+    padding: 10px;
+    overflow: visible;
+    opacity: 1;
+}
+
+    #slider {
+        position: relative;
+        width: 60px;
+        height: 34px;
+        top: 650px;
+        left: 1140px;
+
+    }
+
+    .switch {
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+}
+
+    .switch input { 
+    opacity: 0;
+    width: 0;
+    height: 0;
+}
+
+    .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: .4s;
+    transition: .4s;
+}
+
+    .slider:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+}
+
+    input:checked + .slider {
+  background-color: #2196F3;
+}
+
+    input:focus + .slider {
+    box-shadow: 0 0 1px #2196F3;
+}
+
+    input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+}
+
+/* Rounded sliders */
+    .slider.round {
+    border-radius: 34px;
+}
+
+    .slider.round:before {
+    border-radius: 50%;
+}
 	
 </style>
